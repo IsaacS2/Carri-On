4651cbe5a75ba5677e683be08daf20b507ab8ef4
IsaacS2Hud <sanchezisaac809@gmail.com>
2024-10-09 22:14:20 -0500

    Adds new glide feature
    New glide feature added to VultureGlidingState. When newGlide bool is set to true, the player uses a glide with delayed turning and no downward velocity.

--- Assets/Scripts/Vulture/States/VultureGlidingState.cs
+++ Assets/Scripts/Vulture/States/VultureGlidingState.cs
@@ -8,15 +8,16 @@ public class VultureGlidingState : VultureStateClass
     // Thanks to Jasper Flick for his tutorial for Unity movement,
     // found in Catlike Coding: https://catlikecoding.com/unity/tutorials/movement/sliding-a-sphere/
     // 
-    [SerializeField, Range(0f, 100f)] float maxAcceleration = 4f, glideDownwardForce;
+    [SerializeField, Range(0f, 100f)] private float glideDownwardForce, glideTurnSpeed, newGlideDownwardMultiplier;
+    [SerializeField] private bool newGlide;
+
+    private Vector3 previousForwardDirection;
+    private Vector2 lastMovementDirection, unNormalizedDirection;
+    private float turnPercent;
 
     private void OnEnable()
     {
-        if (_rb != null)
-        {
-            _rb.velocity = Vector3.zero;
-            _rb.useGravity = false;
-        }
+        RestartGlideState();
     }
 
     private void OnDisable()
@@ -27,12 +28,7 @@ public class VultureGlidingState : VultureStateClass
     protected override void Start()
     {
         base.Start();
-
-        if (_rb != null)
-        {
-            _rb.velocity = Vector3.zero;
-            _rb.useGravity = false;
-        }
+        RestartGlideState();
     }
 
     protected override void Update()
@@ -49,14 +45,41 @@ public class VultureGlidingState : VultureStateClass
             isGrounded = Physics.Raycast(_rb.position + new Vector3(0, 0.1f, 0), Vector3.down, out hit, 0.2f) && vultObj.PlatformContact();
             Debug.DrawRay(_rb.position, Vector3.down * hit.distance, Color.green, 1f);
 
-            if (_movementDirection != Vector2.zero)
+            if (newGlide)
             {
-                _rb.transform.forward = new Vector3(_movementDirection.x, 0, _movementDirection.y).normalized;
-            }
+                if (_movementDirection != Vector2.zero)
+                {
+                    if (_movementDirection != lastMovementDirection)
+                    {
+                        previousForwardDirection = _rb.transform.forward;
+                        turnPercent = 0;
+                    }
+                    lastMovementDirection = new Vector2(_movementDirection.x, _movementDirection.y);
+                }
 
-            _rb.velocity = new Vector3(_rb.transform.forward.x * speed * Time.fixedDeltaTime, 
-                _rb.velocity.y, _rb.transform.forward.z * speed * Time.fixedDeltaTime);
-            _rb.AddForce(Vector3.down * glideDownwardForce * Time.fixedDeltaTime, ForceMode.Impulse);
+                Vector2 newDirection = Vector2.Lerp(new Vector2(previousForwardDirection.x, previousForwardDirection.z),
+                    lastMovementDirection, turnPercent);
+                Debug.Log(newDirection);
+                _rb.transform.forward = new Vector3(newDirection.x, 0, newDirection.y);
+
+                _rb.velocity = new Vector3(_rb.transform.forward.x * speed * Time.fixedDeltaTime,
+                    0, _rb.transform.forward.z * speed * Time.fixedDeltaTime);
+                _rb.AddForce(Vector3.down * glideDownwardForce * Time.fixedDeltaTime * newGlideDownwardMultiplier, 
+                    ForceMode.Impulse);
+
+                turnPercent += Time.deltaTime * glideTurnSpeed;
+            }
+            else
+            {
+                if (_movementDirection != Vector2.zero)
+                {
+                    _rb.transform.forward = new Vector3(_movementDirection.x, 0, _movementDirection.y).normalized;
+                }
+
+                _rb.velocity = new Vector3(_rb.transform.forward.x * speed * Time.fixedDeltaTime,
+                    _rb.velocity.y, _rb.transform.forward.z * speed * Time.fixedDeltaTime);
+                _rb.AddForce(Vector3.down * glideDownwardForce * Time.fixedDeltaTime, ForceMode.Impulse);
+            }
         }
     }
 
@@ -81,6 +104,17 @@ public class VultureGlidingState : VultureStateClass
         else
         {
             ChildSwitchState((int)AnimalStates.BeakSlam);
+        }
+    }
+
+    private void RestartGlideState()
+    {
+        if (_rb != null)
+        {
+            _rb.velocity = Vector3.zero;
+            _rb.useGravity = false;
+            previousForwardDirection = _rb.transform.forward;
+            lastMovementDirection = new Vector2(_rb.transform.forward.x, _rb.transform.forward.z);
         }
     }
 }
