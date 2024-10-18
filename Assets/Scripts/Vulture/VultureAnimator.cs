067742e386084580b7a113280aed2c209042b4b4
IsaacS2Hud <sanchezisaac809@gmail.com>
2024-10-17 23:19:58 -0500

    Adds Vulture Model with animations
    Vulture model imported from Blender, with albedo and normal map textures and animations. In the process of creating an animation controller with a script for controlling animations.

---
+++ Assets/Scripts/Vulture/VultureAnimator.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class VultureAnimator : MonoBehaviour
+{
+    [SerializeField] private Animator _anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+}
